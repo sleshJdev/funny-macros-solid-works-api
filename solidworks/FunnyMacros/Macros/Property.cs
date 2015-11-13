716d9c03478f011f403635fa37aec16b1b81b559
sleshJdev <slesh.eugene93@gmail.com>
2015-11-13 03:08:47 +0300

    add enum properties.

---
+++ solidworks/FunnyMacros/Macros/Property.cs
@@ -0,0 +1,17 @@
+﻿namespace FunnyMacros.Macros
+{
+    //names of parameters and mouting planes
+    public enum Property
+    {
+        MAIN_DETAIL_NAME,
+        PARAMETER_CORPUS_WIDTH,
+        PARAMETER_CORPUS_LENGTH,
+        PARAMETER_PLANE_LOCATOR_HEIGHT,
+        PARAMETER_PLANE_LOCATOR_WIDTH,
+        PARAMETER_CYLINDER_LOCATOR_DIAMETER,
+        PARAMETER_CYLINDER_LOCATOR_HEIGHT,
+        MOUNTING_PLANE_NAME,
+        MOUNTING_CYLINDER_PLANE_NAME_1,
+        MOUNTING_CYLINDER_PLANE_NAME_2
+    }
+}
